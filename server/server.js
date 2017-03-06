c54c57da35f9248d61b8b630293b620f00ee0230
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-03-06 00:02:47 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+// serves static files in build folder
+app.use(express.static('../client/build'))
+
+// sends some data..
+app.get('/api/data', function (req, res) {
+  res.json({ some: 'data' })
+})
+
+app.listen(9000, function () {
+  console.log('Example app listening on port 9000!')
+})
